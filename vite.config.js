35e20eadc4c71c1c7ec6ec2f406925d05a73d40b
Aditya Sanjeev Ranjan <asanjeevranjan@gmail.com>
2026-08-17 21:09:31 +0530

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Aditya-s/',
+
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
+
   build: {
-    target: 'esnext'
-  }
+    target: 'esnext',
+  },
 });
